9cce59cc75d1f25524f71a127dbac3eff82e723c
arthur-rosch <rosch.arthur@gmail.com>
2023-04-20 16:23:35 -0300

    Add dependency inversion

---
+++ src/use-cases/register.ts
@@ -0,0 +1,28 @@
+import { hash } from 'bcryptjs'
+import { UsersRepository } from '@/repositories/users-repository'
+
+interface RegisterUserCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({ name, email, password }: RegisterUserCaseRequest) {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new Error('E-mail already exists.')
+    }
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+  }
+}
