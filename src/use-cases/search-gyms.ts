7100005772656de28eec030f3fcee59e414cca52
arthur-rosch <rosch.arthur@gmail.com>
2023-05-16 01:24:34 -0300

    Add Search Gyms Use Case

---
+++ src/use-cases/search-gyms.ts
@@ -0,0 +1,24 @@
+import { Gym } from '@prisma/client'
+import { GymsRepository } from '@/repositories/gyms-repository'
+
+interface SearchGymsUserCaseRequest {
+  query: string
+  page: number
+}
+
+interface SearchGymsUserCaseResponse {
+  gym: Gym[]
+}
+
+export class SearchGymsUseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    query,
+    page,
+  }: SearchGymsUserCaseRequest): Promise<SearchGymsUserCaseResponse> {
+    const gym = await this.gymsRepository.searchMany(query, page)
+
+    return { gym }
+  }
+}
