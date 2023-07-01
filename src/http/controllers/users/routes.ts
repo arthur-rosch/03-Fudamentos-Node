09bf1189418e06ad0bbe81a321c36cebf7b17e17
arthur-rosch <rosch.arthur@gmail.com>
2023-07-01 18:40:45 -0300

    refactoring controller code

---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,13 @@
+import { profile } from './profile'
+import { register } from './register'
+import { FastifyInstance } from 'fastify'
+import { authenticate } from './authenticate'
+import { verifyJwt } from '@/http/middlewares/verify-jwt'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post('/users', register)
+  app.post('/sessions', authenticate)
+
+  /** Authenticated **/
+  app.get('/me', { onRequest: [verifyJwt] }, profile)
+}
