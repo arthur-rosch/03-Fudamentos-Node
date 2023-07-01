ed51aa0f4b5a17439ee9e5dcd3c5eac24deba0d6
arthur-rosch <rosch.arthur@gmail.com>
2023-07-01 03:15:48 -0300

    Add Auth JWT

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,6 +5,7 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.coerce.string(),
+  JWT_SECRET: z.coerce.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
