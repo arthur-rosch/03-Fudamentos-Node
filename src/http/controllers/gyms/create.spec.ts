ecc28daf960e874a3cf96fa81a4baa64f3186a4a
arthur-rosch <rosch.arthur@gmail.com>
2023-07-01 21:02:22 -0300

    Add E2E Test create gym

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,31 @@
+import { app } from '@/app'
+import request from 'supertest'
+import { it, afterAll, beforeAll, describe, expect } from 'vitest'
+import { CreateAndAuthenticate } from '@/utils/test/createAndAuthenticate'
+
+describe('Create Gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create gyms', async () => {
+    const { token } = await CreateAndAuthenticate(app)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'GymsE2E',
+        discription: 'Test E2E',
+        phone: '123456789',
+        latitude: 37.7749,
+        longitude: -122.4194,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
