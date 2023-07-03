032734ffe55c269ba0b410eff71440c9526e7895
arthur-rosch <rosch.arthur@gmail.com>
2023-07-03 14:56:09 -0300

    Add Refresh Token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,4 +1,5 @@
 import { profile } from './profile'
+import { refresh } from './refresh'
 import { register } from './register'
 import { FastifyInstance } from 'fastify'
 import { authenticate } from './authenticate'
@@ -7,6 +8,8 @@ import { verifyJwt } from '@/http/middlewares/verify-jwt'
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   /** Authenticated **/
   app.get('/me', { onRequest: [verifyJwt] }, profile)
